9ae89b0cf5a42e437585f67832fb034aa8a92a1c
Xrenore3 <62048261+Xrenore3@users.noreply.github.com>
2020-11-07 21:26:12 +0200

    Updates

---
+++ precache-manifest.3158fe9d65550baf3ed4246b07f09585.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc6fa2029b038ec6845a8d407a12fe52",
+    "url": "/Basic-project_groceru_bud/index.html"
+  },
+  {
+    "revision": "284c815cb927e75ca009",
+    "url": "/Basic-project_groceru_bud/static/css/main.a903c182.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/Basic-project_groceru_bud/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Basic-project_groceru_bud/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "284c815cb927e75ca009",
+    "url": "/Basic-project_groceru_bud/static/js/main.be93dcf0.chunk.js"
+  },
+  {
+    "revision": "a19db2c839ee8fa01824",
+    "url": "/Basic-project_groceru_bud/static/js/runtime-main.dc7db746.js"
+  }
+]);
